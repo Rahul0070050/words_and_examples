51038f1ea3a0db10e5bad8516ff8061d4af81241
rahulor0070050 <rahulor2000@gmail.com>
2022-10-06 22:18:56 +0530

    finished

--- app.js
+++ app.js
@@ -1,11 +1,58 @@
+let fact = null;
+let key = null;
+let have = false;
+
+const savedFacts = document.getElementById('saved-facts-list')
 function shoRandomResult() {
+
     fetch('https://asli-fun-fact-api.herokuapp.com/')
         .then(response => response.json())
         .then(response => {
             document.getElementById('fact').innerText = response.data.fact
+            fact = response.data.fact
+            key = new Date().getTime()
+            have = true;
         })
         .catch(err => console.error(err));
 }
+// localStorage.clear('fact')
+
+let facts = JSON.parse(localStorage.getItem('fact'))
+facts?.forEach(fact => {
+    savedFacts.innerHTML += `<li class="list-group-item px-3 border-0 rounded-3 list-group-item-primary mb-2"><h6 id="text">${fact.fact}</h6><h6 id="delete" onClick="deleteFact(${fact.key})"><img src="https://img.icons8.com/ios-glyphs/90/000000/delete-sign.png"/></h6></li>`
+});
+
+function saveTheFact() {
+    if (!have) {
+        return
+    }
+    have = false
+
+    if (facts) {
+        console.log(facts);
+        localStorage.clear('fact')
+        facts.push({ key, fact })
+        savedFacts.innerHTML += `<li class="list-group-item px-3 border-0 rounded-3 list-group-item-primary mb-2"><h6 id="text">${fact}</h6><h6 id="delete" onClick="deleteFact(${key})"><img src="https://img.icons8.com/ios-glyphs/90/000000/delete-sign.png"/></h6></li>`
+        localStorage.setItem('fact', JSON.stringify(facts))
+    } else {
+        let facts = [{ key, fact }]
+        savedFacts.innerHTML = `<li class="list-group-item px-3 border-0 rounded-3 list-group-item-primary mb-2"><h6 id="text">${fact}</h6><h6 id="delete" onClick="deleteFact(${key})"><img src="https://img.icons8.com/ios-glyphs/90/000000/delete-sign.png"/></h6></li>`
+        localStorage.setItem('fact', JSON.stringify(facts))
+    }
+}
+
+function deleteFact(key) {
+    let facts = JSON.parse(localStorage.getItem('fact'))
+    let tempFact = facts?.filter(fact => fact.key != key)
+    localStorage.setItem('fact',JSON.stringify(tempFact))
+    savedFacts.innerHTML = ""
+    tempFact?.forEach(fact => {
+        savedFacts.innerHTML += `<li class="list-group-item px-3 border-0 rounded-3 list-group-item-primary mb-2"><h6 id="text">${fact.fact}</h6><h6 id="delete" onClick="deleteFact(${fact.key})"><img src="https://img.icons8.com/ios-glyphs/90/000000/delete-sign.png"/></h6></li>`
+    });
+}
+
+
+
 shoRandomResult()
 
 // do your browser supports service worker
