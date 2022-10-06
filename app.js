bbc1ec6b37c3c994077c3d29dec9f1c37c9302fd
rahulor0070050 <rahulor2000@gmail.com>
2022-10-06 20:28:59 +0530

    almost finished

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-navigator.serviceWorker.register('./app.service.worker.js').then(res => {
-    console.log('servese worker is done');
-}).catch(err => {
-    console.log(err);
-})
+function shoRandomResult() {
+    fetch('https://asli-fun-fact-api.herokuapp.com/')
+        .then(response => response.json())
+        .then(response => {
+            document.getElementById('fact').innerText = response.data.fact
+        })
+        .catch(err => console.error(err));
+}
+shoRandomResult()
+
+// do your browser supports service worker
+if ("serviceWorker" in navigator) {
+    // register the service worker file
+    navigator.serviceWorker.register('./app.service.worker.js').then(res => { // success
+        console.log('servese worker is done');
+    }).catch(err => { // failed
+        console.log(err);
+    })
+}
--- app.service.worker.js
+++ app.service.worker.js
@@ -1,1 +1,53 @@
+// cashe name
 const CATCHE_NAME = "rahuls catche"
+
+// put the files toor for caching 
+const cacheFiles = [
+    "./app.css",
+    "./index.html",
+    "./app.js",
+    "manifest.json",
+    "download.webp"
+];
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(caches.open(CATCHE_NAME).then(function (cache) {
+        console.log('cache opend');
+        return cache.addAll(cacheFiles)
+    }))
+})
+self.addEventListener("activate", function (event) {
+    console.log('service worker is activeted');
+})
+self.addEventListener("fetch",function (event) {
+    event.respondWith(caches.match(event.request).then(response => {
+        if(response) {
+            return response
+        } else {
+            return fetch(event.request)
+        }
+    }))
+})
+
+// self.addEventListener('fetch',function (event) {
+//     event.respondWith(
+//         caches.match(event.request).then(function (response)  {
+//             if(response) {
+//                 return response
+//             }
+//             return fetch(event.request).then(function (response) {
+//                 if(!response || response.status !== 200 || response.type !== "basic") {
+//                     return response
+//                 }
+
+//                 let responseToCatch = response.clone()
+
+//                 caches.open(CATCHE_NAME).then(function (cache) {
+//                     cache.put(event.request,responseToCatch)
+//                 })
+
+//                 return response
+//             })
+//         })
+//     )
+// })
