c387b89fd564de21394e52c16c0404b25e80d37b
rahulor0070050 <rahulor2000@gmail.com>
2022-10-10 14:03:00 +0530

    alert message updated

--- app.js
+++ app.js
@@ -55,9 +55,6 @@ whatsApp.addEventListener('click', () => {
     a.click()
 })
 
-var winFeature = 'location=no,toolbar=no,menubar=no,scrollbars=yes,resizable=yes';
-window.open('Result.html', 'null', winFeature);
-
 
 
 function shoRandomResult() {
